3a59e302875301949f873f3051f91fa941aac41a
Waqar <urspecial1one@gmail.com>
2023-03-30 02:28:40 +0500

    Lesson 8

---
+++ Leason-8/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
+        }
+        // brand: "#0fa9e6",
+        // "brand-light":"#3fbaeb",
+        // "brand-dark":"#0c87b8",
+      },
+      fontFamily:{
+        headline:" Poppins,sans-serif" //font-headline
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+}
